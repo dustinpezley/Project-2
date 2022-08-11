31449e91e9363f8f263e55c61e0a8837fb1a41fd
Griffin <griffinlebeau2@gmail.com>
2022-08-10 23:06:45 -0500

    models completed

--- models/Venue.js
+++ models/Venue.js
@@ -1,4 +1,31 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
+class Venue extends Model {}
+
+Venue.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        address: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'venue'
+    }
+)
+
 module.exports = Venue;
