9689d75fc89a9942ede097d290d8fc48da0fd097
Andrew Seymour <andrew.seymour1@gmail.com>
2022-08-12 14:11:07 -0500

    sales model

--- models/Sales.js
+++ models/Sales.js
@@ -1,10 +1,36 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
-class Sales extends Model {};
+class Sales extends Model {}
 
-Sales.init({
-    
-})
+Sales.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    payment_type: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    amount: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+    },
+    product_type: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    paranoid: true,
+    modelName: "sales",
+  }
+);
 
 module.exports = Sales;
