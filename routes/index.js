75b095da25e3aa83697fc3618eba619aa74cee37
Andrew Seymour <andrew.seymour1@gmail.com>
2022-08-14 16:57:11 -0500

    added index route details, modified talent.js

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,11 @@
-const router = require("express").Router();
+const Employee = require("./Employee");
+const Role = require("./Role");
+const Sales = require("./Sales");
+const Talent = require("./Talent");
+const User = require("./User");
+const Venue = require("./Venue");
+
+// create associations between models
+Employee.hasMany(Role, {
+  foreignKey,
+});
