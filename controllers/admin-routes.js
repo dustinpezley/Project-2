a590ad84e6810dfe4a0aa2efa3d62ea15b29315d
dustinpezley <dustinpezley@gmail.com>
2022-08-18 19:45:34 -0500

    returning employee data on admin screen

--- controllers/admin-routes.js
+++ controllers/admin-routes.js
@@ -56,6 +56,7 @@ router.get('/', withAuth, (req, res) => {
     )
     .then((dbData) => {
       const fullData = dbData.map((data) => data.get({ plain: true }));
+      console.log(fullData);
       res.render('admin', {
         fullData,
         title: 'Polyrhythm Admin',
@@ -64,5 +65,73 @@ router.get('/', withAuth, (req, res) => {
     })
     .catch((err) => res.status(500).json(err));
 });
+
+// router.get('/', withAuth, (req, res) => {
+//   console.log(req.session);
+//   console.log('========================');
+
+//   var findAllEmployees = Employee.findAll({
+//     attributes: ['first_name', 'last_name'],
+//     order: ['last_name'],
+//     include: [
+//       {
+//         model: User,
+//         attributes: ['username', 'email', 'admin'],
+//       },
+//       {
+//         model: Role,
+//         attributes: ['name', 'salary', 'salary_rate'],
+//       },
+//       {
+//         model: Venue,
+//         attributes: ['name'],
+//       },
+//     ],
+//   });
+
+//   var findAllRoles = Role.findAll({
+//     attributes: ['name', 'salary_rate', 'salary'],
+//   });
+
+//   var findAllVenues = Venue.findAll({
+//     attributes: ['name', 'address', 'capacity'],
+//   });
+
+//   var findAllTalent = Talent.findAll({
+//     attributes: ['title'],
+//   });
+
+//   var findAllShows = Shows.findAll({
+//     attributes: ['performance_date', 'performance_time'],
+//     include: [
+//       {
+//         model: Talent,
+//         attributes: ['title'],
+//       },
+//       {
+//         model: Venue,
+//         attributes: ['name', 'address', 'capacity'],
+//       },
+//     ],
+//   });
+
+//   Promise.all([
+//     findAllEmployees,
+//     findAllRoles,
+//     findAllShows,
+//     findAllTalent,
+//     findAllVenues,
+//   ])
+//     .then((dbData) => {
+//       const fullData = dbData.map((data) => data.get({ plain: true }));
+//       console.log(fullData);
+//       res.render('admin', {
+//         fullData,
+//         title: 'Polyrhythm Admin',
+//         loggedIn: true,
+//       });
+//     })
+//     .catch((err) => res.status(500).json(err));
+// });
 
 module.exports = router;
